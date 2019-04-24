1043f76b5f7449ea8ccc11315fc2cff2f90be933
Gustavo Quenca <gustavo.quenca@icloud.com>
2019-04-24 11:29:50 -0300

    Creating the Project

---
+++ BTG/BTG/View Controller/FavoritesViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FavoritesViewController.swift
+//  BTG
+//
+//  Created by Gustavo Quenca on 23/04/19.
+//  Copyright © 2019 Quenca. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
