67c08382545e48b6ecef2b7f7c1dfc63ef83c3b4
Gustavo Quenca <gustavo.quenca@icloud.com>
2019-04-24 11:59:35 -0300

    NothingFound error

---
+++ BTG/BTG/View/NothingFoundCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  NothingFoundCollectionViewCell.swift
+//  BTG
+//
+//  Created by Gustavo Quenca on 24/04/19.
+//  Copyright © 2019 Quenca. All rights reserved.
+//
+
+import UIKit
+
+class NothingFoundCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
